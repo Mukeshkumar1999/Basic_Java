c1f17431797f4213dfebf8d367935f94792ebb1c
Mukesh kumar paswan <mukeshk92578@gamil.com>
2023-11-01 15:52:09 +0530

    01/11/2023 leetcode 523

---
+++ src/Leetcode/ContinuoseSubArraySum523.java
@@ -0,0 +1,28 @@
+package Leetcode;
+import java.util.*;
+public class ContinuoseSubArraySum523 {
+    public static boolean continuose(int[]arr , int k ){
+        int MinLen = arr.length;
+        for(int idx = 0; idx < arr.length-1; idx++){
+            int sum = arr[idx];
+            for(int jdx = idx+1; jdx < arr.length; jdx++){
+                sum += arr[jdx];
+                if(sum % k == 0){
+                    MinLen = Math.min(MinLen, jdx - idx +1);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[] arr = new int[n];
+        for(int idx = 0; idx < n; idx++){
+            arr[idx] = scn.nextInt();
+        }
+        System.out.println(continuose(arr, m));
+    }
+}
